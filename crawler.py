1843a8c35388c4e8e9bf737bf2a661a795b2d7b1
MiedzinskiBuck <andre.miedzinski@nubank.com.br>
2019-10-24 17:38:19 -0300

    Few improvements

--- crawler.py
+++ crawler.py
@@ -34,7 +34,7 @@ def parser(target_url):
     for link in href_links:
         link = urllib.parse.urljoin(target_url, link)
         target_links.append(link)
-        print("\033[0;32;40m" + link)
+        print("\033[0;32;40m{0}".format(link))
 
 def subdomain_discovery(target_url, wordlist):
     print("\033[0;33;40m[+]Do you want to run a subdomain discovery?[+]\n")
@@ -50,10 +50,10 @@ def subdomain_discovery(target_url, wordlist):
         with open(wordlist, "r") as wordlist_file:
             for line in wordlist_file:
                 word = line.strip()
-                test_url = "https://" + word + "." + target_url
+                test_url = "https://{0}.{1}".format(word, target_url)
                 response = request(test_url)
                 if response:
-                    print("\033[0;32;40m[+] Discovered subdomain --> " + test_url)
+                    print("\033[0;32;40m[+] Discovered subdomain --> {0}".format(test_url))
 
     print("\033[0;34;40m[+] Subdomain searching finished with success! [+]\n")
 
@@ -66,20 +66,20 @@ def directory_discovery(target_url, wordlist):
         with open(wordlist, "r") as directory_file:
             for line in directory_file:
                 directory = line.strip()
-                test_directory = target_url + "/" + directory
+                test_directory = "{0}/{1}".format(target_url, directory)
                 response = requests.get(test_directory)
                 if response:
-                    print("\033[0;32;40m[+] Discovered URL --> " + test_directory)
+                    print("\033[0;32;40m[+] Discovered URL --> {0}".format(test_directory))
                     discovered_directories.append(test_directory)
             for new_directory in discovered_directories:
-                print("\033[0;34;40m\n### Entering " + new_directory + "###\n")
+                print("\033[0;34;40m\n### Entering {0} ###\n".format(new_directory))
                 with open(wordlist, "r") as new_directory_file:
                     for l in new_directory_file:
                         subdirectory = line.strip()
                         new_test_directory = new_directory + subdirectory
                         response = requests.get(new_test_directory)
                         if response:
-                            print("\033[0;32;40m[+] Discovered a new URL --> " + new_test_directory)
+                            print("\033[0;32;40m[+] Discovered a new URL --> {}".format(new_test_directory))
                             discovered_directories.append(new_test_directory)
 
     print("\033[0;34,40m\n[+] Directory searching finished with succes! [+]\n")
@@ -95,19 +95,19 @@ def file_discovery(target_url, wordlist, filelist):
         print("\033[0;33;40mWhat extension file do you want to search for?")
         extension = input("--> ")
         if not filelist:
-            print("\033[0;33;40m[+]No File List specified, using the same wordlist: " + wordlist + " [+]")
+            print("\033[0;33;40m\n[+]No File List specified, using the same wordlist: {0} [+]".format(wordlist))
             wlist = wordlist
         else:
-            print("\033[0;33;40m[+]Starting file discovery...[+]")
             wlist = filelist
-            with open(wlist, "r") as filename_file:
-                for line in filename_file:
-                    filename = line.strip()
-                    test_file = target_url + "/" + filename + extension 
-                    file_reach = requests.get(test_file)
-                    if file_reach:
-                        print("\033[0;32;40m[+] Discovered File--> " + test_file)
-                        discovered_files.append(test_file)
+        print("\033[0;33;40m[+]Starting file discovery...[+]")
+        with open(wlist, "r") as filename_file:
+            for line in filename_file:
+                filename = line.strip()
+                test_file = "{0}/{1}{2}".format(target_url, filename, extension) 
+                file_reach = requests.get(test_file)
+                if file_reach:
+                    print("\033[0;32;40m[+] Discovered File--> {}".format(test_file))
+                    discovered_files.append(test_file)
 
     print("\033[0;34,40m[+] File searching finished with succes! [+]")
 
